a823ec53c9309750180cae90779a1f2362accfd4
STEPS0N <stepan.klimoff@mail.ru>
2026-05-20 18:19:28 +0500

    Созданный класс Config. Климов

---
+++ TaskManager_Klimov/Classes/Database/Config.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_Klimov.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=localhost;" +
+            "port=3307;" +
+            "uid=root;" +
+            "pwd=;" +
+            "database=TaskManager;";
+
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
