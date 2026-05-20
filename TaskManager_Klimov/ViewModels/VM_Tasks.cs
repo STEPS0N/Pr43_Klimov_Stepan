988366baa87df038fd52100ff175b8f5ad24e8d3
STEPS0N <stepan.klimoff@mail.ru>
2026-05-20 19:02:21 +0500

    Разработанная модель представления главного окна. Климов

---
+++ TaskManager_Klimov/ViewModels/VM_Tasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_Klimov.Classes;
+
+namespace TaskManager_Klimov.ViewModels
+{
+    public class VM_Tasks : Notification
+    {
+
+    }
+}
