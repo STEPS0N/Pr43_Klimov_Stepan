d8973d1d34f5cc7bf77900fb4f45c853620a5b61
STEPS0N <stepan.klimoff@mail.ru>
2026-05-20 18:58:24 +0500

    Разработанный контекст данных для модели задач. Климов

---
+++ TaskManager_Klimov/Context/TasksContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_Klimov.Models;
+
+namespace TaskManager_Klimov.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Classes.Database.Config.connection,
+                Classes.Database.Config.version);
+        }
+    }
+}
